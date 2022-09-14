ad624281189d4a66d47f3f0f78f4f5a49e93effb
Ariya kusumaa <ariya.as19@gmail.com>
2022-09-14 22:54:41 +0700

    tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_CatalogItem = CatalogItem.objects.all()
+    context = {
+        'list_Catalog': data_CatalogItem,
+        'name': 'Ariya',
+        'studentid' : '2106751991'
+    }
+    return render(request, "katalog.html", context)
